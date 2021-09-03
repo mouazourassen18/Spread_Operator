8c1478976f1f591d129ccc47d39fa7dd42e17b8a
mouazourassen18 <54610297+mouazourassen18@users.noreply.github.com>
2021-09-03 19:42:14 -0400

    finalVersion

--- src/index.js
+++ src/index.js
@@ -3,3 +3,18 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+const one = [1, 2, 3, 4, 5];
+const two = [...one, 6, 7, 8, 9];
+console.log(two);
+// the same thing for adding objects
+
+const ouazou = {
+  fname: "mohamed",
+  lname: "ouazourassen"
+};
+const rassen = {
+  age: "22",
+  email: "oua99@",
+  ...ouazou
+};
+console.log(rassen);
